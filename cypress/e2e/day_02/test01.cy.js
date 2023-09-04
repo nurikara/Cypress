59659aaa5688af6171adb78c06b83390cf37c5d7
nuri Kara <nurikara@yahoo.com>
2023-09-05 01:09:56 +0300

    Merhaba

--- cypress/e2e/day_02/test01.cy.js
+++ cypress/e2e/day_02/test01.cy.js
@@ -5,7 +5,16 @@ describe('Koala Resort Hotel',   () => {
 
     it('Log testi', () => {
 
+        cy.visit('https://www.koalaresorthotels.com/')
 
+       // cy.get('#navLogon > .nav-link').click()
+cy.contains('Log in').click()
+
+cy.url().should('include','koalaresorthotels')
+
+cy.get('#UserName').type('Manager')
+cy.get('#Password').type('Manager1!')
+cy.get('#btnSubmit').click()
 
 })
 
