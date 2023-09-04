597e4d204ea0f31a6b6d5ac0b3871bc14276aed6
nuri Kara <nurikara@yahoo.com>
2023-09-05 01:22:53 +0300

    ed

---
+++ cypress/e2e/day_02/test02.cy.js
@@ -0,0 +1,18 @@
+/// <reference types='cypress'/>
+
+describe('', function () {
+
+it('Login Test', () => {
+
+    cy.visit('https://www.koalaresorthotels.com/')
+
+    cy.get('#navLogon > .nav-link').click()
+
+
+
+
+    
+});
+
+
+})
