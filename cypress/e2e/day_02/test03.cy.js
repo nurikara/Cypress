72bf050004032f14ea697d7ea59a704fe507ddd8
nuri Kara <nurikara@yahoo.com>
2023-09-05 19:57:57 +0300

    q

--- cypress/e2e/day_02/test03.cy.js
+++ cypress/e2e/day_02/test03.cy.js
@@ -1,1 +1,16 @@
 /// <reference types='cypress'/>
+
+describe('',()=>{
+
+    it('Test Case 1', () => {
+        cy.visit('')
+        
+    });
+
+    it('Test Case 2', () => {
+        
+    });
+
+
+
+})
