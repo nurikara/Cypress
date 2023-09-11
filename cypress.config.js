3fd55454c259ffbc437323e6f81708462cfadaf1
nuri Kara <nurikara@yahoo.com>
2023-09-11 21:10:26 +0300

    as

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'u2nbt1',
+  projectId: 'u2nbt1',  
+ 
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://www.amazon.com"
-  },
+    baseUrl: "http://www.amazon.com",
+     
+    projectId: "1jto35",
+  "video": false  },
 });
